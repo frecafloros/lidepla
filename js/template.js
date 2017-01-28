2ad1df42cdbb044a567faee1f10988107151a096
frecafloros <frecafloros@gmail.com>
2017-01-28 20:56:34 +0900

    jsの導入

---
+++ js/template.js
@@ -0,0 +1,11 @@
+function footer(){
+  var html="";
+
+  html += '<footer>';
+  html += '<div class="container">';
+  html += '<br>© 2017 frecafloros';
+  html += '</div>';
+  html += '</footer>';
+
+  document.write(html);
+}
