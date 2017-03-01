1b2eae6324dfde3ff2b03b7d4d05ab98954dcfe0
frecafloros <frecafloros@gmail.com>
2017-03-02 00:46:04 +0900

    第2課
    第2課およびマイナーチェンジ

--- js/template.js
+++ js/template.js
@@ -1,3 +1,30 @@
+function menubar(){
+  var html="";
+
+  html += '<nav>';
+    html += '<div id="menu-common">';
+      html += '<div id="menu-first">';
+        html += '<ul>';
+          html += '<li><a href="/lidepla/index.html">TOP</a></li>';
+        html += '</ul>';
+      html += '</div>';
+    html += '</div>';
+    html += '<br>';
+    html += '初級編';
+    html += '<div id="menu-syokyu">';
+      html += '<div id="menu-first">';
+        html += '<ul>';
+          html += '<li><a href="/lidepla/leson/1-01.html">1 はじめに</a></li>';
+          html += '<li><a href="/lidepla/leson/1-02.html">2 文字と発音</a></li>';
+          html += '<li><a href="">3 基本的な文の構造</a></li>';
+        html += '</ul>';
+      html += '</div>';
+    html += '</div>';
+  html += '</nav>';
+
+  document.write(html);
+}
+
 function footer(){
   var html="";
 
